3a49df73bef0c7abd62f4505db79d72ab578ccbb
Alexey_Kashkov <kashkov.alexey@gmail.com>
2019-02-11 23:00:37 +0300

    add

---
+++ lesson-4/Varible.java
@@ -0,0 +1,21 @@
+public class Varible {	
+    public static void main(String[] args) {
+    	byte Parameter1 = 100;
+    	short Parameter2 = 200;
+    	int Parameter3 = 200000;
+    	long Parameter4 = 2147483647;
+    	float Parameter5 = 2.5f;
+    	double Parameter6 = 2.554;
+    	char Parameter7 = '\u0042';
+    	boolean Parameter8 = true;
+
+       System.out.println("Parameter1="+Parameter1);
+       System.out.println("Parameter2="+Parameter2);
+       System.out.println("Parameter3="+Parameter3);
+       System.out.println("Parameter4="+Parameter4);
+       System.out.println("Parameter5="+Parameter5);
+       System.out.println("Parameter6="+Parameter6);
+       System.out.println("Parameter7="+Parameter7);
+       System.out.println("Parameter8="+Parameter8);
+   }
+}
